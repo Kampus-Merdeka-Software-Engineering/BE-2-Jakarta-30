6940a1328a7d483d418ca5dc6bc1ebaa587f1eac
Qois Salsabila <damayantialfina45@gmail.com>
2023-12-05 17:15:50 +0700

    BE 05

---
+++ src/modules/database.js
@@ -0,0 +1,139 @@
+import { Sequelize, DataTypes } from "sequelize";
+import {} from "dotenv/config";
+
+console.log(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_HOST
+);
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: "mysql",
+  }
+);
+
+const News = sequelize.define("News", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  category_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    foreignKey: true,
+  },
+  thumbnail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  thumbnail_description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const Categories = sequelize.define("Categories", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const Recommendation = sequelize.define("Recommendations", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  category_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    foreignKey: true,
+  },
+  news_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    foreignKey: true,
+  },
+});
+
+const Trendings = sequelize.define("Trendings", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  news_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    foreignKey: true,
+  },
+  trending_rank: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+// News & Categories
+Categories.hasMany(News, {
+  foreignKey: "category_id",
+});
+News.belongsTo(Categories, {
+  foreignKey: "category_id",
+  targetKey: "id",
+  as: "category",
+});
+
+// News & Recommendation
+News.hasOne(Recommendation, {
+  foreignKey: "news_id",
+});
+Recommendation.belongsTo(News, {
+  foreignKey: "news_id",
+  targetKey: "id",
+});
+
+// // News and Trending
+News.hasOne(Trendings, {
+  foreignKey: "news_id",
+});
+Trendings.belongsTo(News, {
+  foreignKey: "news_id",
+  targetKey: "id",
+});
+
+(async () => {
+  await sequelize.sync();
+})();
+
+export { sequelize, News, Categories, Recommendation, Trendings };
---
+++ src/services/news.js
@@ -0,0 +1,56 @@
+import {
+  createNews,
+  getNews,
+  getNewsByID,
+  getNewsBySlug,
+  getNewsSearchByTitle,
+  createNewsBulk,
+} from "../modules/news.js";
+// import { getCommentsByNewsID } from "../modules/comments.js";
+
+export const getNewsService = async (req, res) => {
+  const news = await getNews({
+    category_id: Number(req.query.category_id),
+    limit: Number(req.query.limit),
+  });
+
+  res.send(news);
+};
+
+export const getNewsBySearchService = async (req, res) => {
+  const news = await getNewsSearchByTitle({
+    keyword: req.query.keyword,
+    limit: Number(req.query.limit),
+  });
+  res.send(news);
+};
+
+export const getNewsByIDService = async (req, res) => {
+  const news = await getNewsByID(Number(req.params.news_id));
+  res.send(news);
+};
+
+export const getNewsBySlugService = async (req, res) => {
+  const news = await getNewsBySlug(req.params.slug);
+  res.send(news);
+};
+
+// export const getNewsCommentsService = async (req, res) => {
+//   const comments = await getCommentsByNewsID({
+//     news_id: Number(req.params.id),
+//     limit: Number(req.query.limit),
+//   });
+//   res.send(comments);
+// };
+
+export const createNewsService = async (req, res) => {
+  const createdNews = await createNews(req.body);
+
+  res.json(createdNews);
+};
+
+export const createNewsBulkService = async (req, res) => {
+  const createdNews = await createNewsBulk(req.body);
+
+  res.json(createdNews);
+};
