5aeec8f6d395d830c67915011001a6f7b0592cf9
Fatikhur <fatikhuralfa@gmail.com>
2023-12-11 14:44:28 +0700

    perbarui

--- server.js
+++ server.js
@@ -22,6 +22,160 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello Cek" });
 });
 
+app.get("/recapi", async (req, res) => {
+
+    try {
+        // Assume the server has an endpoint /data that you want to fetch
+        const externalApiResponse = await fetch('https://newsapi.org/v2/everything?q=scenery&sortBy=relevancy&pageSize=15&apiKey=d3d925f2f29d43149491530eab2d51b4', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Additional headers if needed
+          },
+        });
+    
+        if (!externalApiResponse.ok) {
+          throw new Error('Failed to fetch data from external API');
+        }
+    
+        const externalApiData = await externalApiResponse.json();
+    
+        // Process or send data to the client
+        res.json({ success: true, data: externalApiData });
+      } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+      }
+    });
+
+app.get("/recapi", async (req, res) => {
+
+    try {
+        // Assume the server has an endpoint /data that you want to fetch
+        const externalApiResponse = await fetch('https://newsapi.org/v2/everything?q=scenery&sortBy=relevancy&pageSize=15&apiKey=d3d925f2f29d43149491530eab2d51b4', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Additional headers if needed
+          },
+        });
+    
+        if (!externalApiResponse.ok) {
+          throw new Error('Failed to fetch data from external API');
+        }
+    
+        const externalApiData = await externalApiResponse.json();
+    
+        // Process or send data to the client
+        res.json({ success: true, data: externalApiData });
+      } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+      }
+    });
+
+// main
+app.get("/recapi", async (req, res) => {
+
+    try {
+        // Assume the server has an endpoint /data that you want to fetch
+        const externalApiResponse = await fetch('https://newsapi.org/v2/everything?q=random&sortBy=relevancy&pageSize=15&apiKey=d3d925f2f29d43149491530eab2d51b4', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Additional headers if needed
+          },
+        });
+    
+        if (!externalApiResponse.ok) {
+          throw new Error('Failed to fetch data from external API');
+        }
+    
+        const externalApiData = await externalApiResponse.json();
+    
+        // Process or send data to the client
+        res.json({ success: true, data: externalApiData });
+      } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+      }
+}); 
+
+//nature
+app.get("/nature", async (req, res) => {
+
+    try {
+        // Assume the server has an endpoint /data that you want to fetch
+        const externalApiResponse = await fetch('https://newsapi.org/v2/everything?q=scenery&sortBy=relevancy&pageSize=15&apiKey=d3d925f2f29d43149491530eab2d51b4', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Additional headers if needed
+          },
+        });
+    
+        if (!externalApiResponse.ok) {
+          throw new Error('Failed to fetch data from external API');
+        }
+    
+        const externalApiData = await externalApiResponse.json();
+    
+        // Process or send data to the client
+        res.json({ success: true, data: externalApiData });
+      } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+      }
+});
+
+//Photography
+app.get("/photo", async (req, res) => {
+
+    try {
+        // Assume the server has an endpoint /data that you want to fetch
+        const externalApiResponse = await fetch('https://newsapi.org/v2/everything?q=photography&sortBy=relevancy&pageSize=15&apiKey=d3d925f2f29d43149491530eab2d51b4', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Additional headers if needed
+          },
+        });
+    
+        if (!externalApiResponse.ok) {
+          throw new Error('Failed to fetch data from external API');
+        }
+    
+        const externalApiData = await externalApiResponse.json();
+    
+        // Process or send data to the client
+        res.json({ success: true, data: externalApiData });
+      } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+      }
+});
+
+//Tech
+app.get("/tech", async (req, res) => {
+
+    try {
+        // Assume the server has an endpoint /data that you want to fetch
+        const externalApiResponse = await fetch('https://newsapi.org/v2/everything?q=technology&sortBy=relevancy&pageSize=15&apiKey=d3d925f2f29d43149491530eab2d51b4', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Additional headers if needed
+          },
+        });
+    
+        if (!externalApiResponse.ok) {
+          throw new Error('Failed to fetch data from external API');
+        }
+    
+        const externalApiData = await externalApiResponse.json();
+    
+        // Process or send data to the client
+        res.json({ success: true, data: externalApiData });
+      } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+      }
+});
+
 require('./app/routes/auth.routes')(app);
 require('./app/routes/post.routes')(app);
 
